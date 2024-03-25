403f176eeaf2f1cd7b78bb7bf9ff3d78bb876e99
Aboubacar <petit.toure@gmail.com>
2024-03-25 09:03:36 +0100

    check problem api get petitions

--- src/main/java/foo/PetitonEndpoint.java
+++ src/main/java/foo/PetitonEndpoint.java
@@ -34,7 +34,7 @@ import java.util.List;
 
 public class PetitonEndpoint {
 
-    @ApiMethod(name = "listpetition", httpMethod = HttpMethod.GET)
+    @ApiMethod(name = "petions", httpMethod = HttpMethod.GET)
     public List<Entity> listpetition() {
         /*    throws UnauthorizedException {
         if (user == null) {
--- src/main/webapp/petitions/components/petitionsComponent.jsx
+++ src/main/webapp/petitions/components/petitionsComponent.jsx
@@ -19,7 +19,7 @@ export default class PetitionsComponent {
         ];
         return m.request({
             method: "GET",
-            url: "/_petition/api/apiPetition/v1/listpetition"
+            url: "/_petition/api/apiPetition/v1/petitions"
         })
             .then((res) => {
                 console.log(res);
